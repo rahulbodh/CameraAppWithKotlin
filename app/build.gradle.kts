5d474b2cad55a00dcf2c08ed4c13ab4324bfe529
rahulbodh <rahulbodh819@gmail.com>
2025-09-24 17:21:45 +0530

    Add some pngs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     implementation ("androidx.camera:camera-video:${camerax_version}")
     implementation ("androidx.camera:camera-view:${camerax_version}")
     implementation ("androidx.camera:camera-extensions:${camerax_version}")
+
+    implementation("com.google.mlkit:face-mesh-detection:16.0.0-beta1")
 }
